c99c2f952cfced01f2d7ebbf382d468aab5e2e75
qui2dty <96517530+qui2dty@users.noreply.github.com>
2024-12-17 01:20:06 +0900

    fix: 마나 위치 가시성 수정

--- Assets/jsResource/CameraShake.cs
+++ Assets/jsResource/CameraShake.cs
@@ -4,27 +4,5 @@ using Unity.Cinemachine;
 
 public class CameraShake : MonoBehaviour
 {
-    public IEnumerator Shake(float duration, float magnitude)
-    {
-        Vector3 originalPosition = transform.localPosition;
-        Debug.Log("Original Position: " + originalPosition); // 디버그 로그 추가
 
-        float elapsed = 0.0f;
-
-        while (elapsed < duration)
-        {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-
-            transform.localPosition = new Vector3(x, y, originalPosition.z);
-            Debug.Log("Shaking: x=" + x + ", y=" + y); // 디버그 로그 추가
-
-            elapsed += Time.deltaTime;
-
-            yield return null;
-        }
-
-        transform.localPosition = originalPosition;
-        Debug.Log("Position Reset: " + originalPosition); // 디버그 로그 추가
-    }
 }
