b6dcf9c6657ff5a3f32478c6bbc60d6e2af716c3
LeeSanghyun1212 <gustkd7125@gmail.com>
2024-12-21 22:20:27 +0900

    feat : ip 인스턴스로 변경
    feat : ip 인스턴스로 변경

--- Assets/_Project/Scripts/UI/Popup/PopupLogin.cs
+++ Assets/_Project/Scripts/UI/Popup/PopupLogin.cs
@@ -52,7 +52,7 @@ public class PopupLogin : UIBase
         {
             //var ip = PlayerPrefs.GetString("ip", "127.0.0.1");
             //var port = PlayerPrefs.GetString("port", "9000");
-            var ip = "127.0.0.1";
+            var ip = "15.165.168.120";
             var port = "9000";
             SocketManager.instance.Init(ip, int.Parse(port));
             SocketManager.instance.Connect(() =>
@@ -74,7 +74,7 @@ public class PopupLogin : UIBase
         ClickSound();
         if (!SocketManager.instance.isConnected)
         {
-            var ip = "127.0.0.1";
+            var ip = "15.165.168.120";
             var port = "9000";
             SocketManager.instance.Init(ip, int.Parse(port));
             SocketManager.instance.Connect(() =>
